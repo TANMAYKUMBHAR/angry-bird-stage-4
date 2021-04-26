3e70eca48e8d1cc1b518c24571140515d1a296de
TANMAYKUMBHAR <75516143+TANMAYKUMBHAR@users.noreply.github.com>
2021-04-26 14:54:19 +0530

    Add files via upload

--- Slingshot.js
+++ Slingshot.js
@@ -25,9 +25,22 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
+            push();
             strokeWeight(4);
             stroke(102, 23, 23)
-           // line(pointA.x, pointA.y, pointB.x, pointB.y);
+            if(pointA.x<220){
+                strokeWeight(7);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20,pointA.y,pointB.x+30,pointB.y-3);
+                image(this.sling3,pointA.x-30,pointA.y-10,15,30);
+            }
+           else{
+               strokeWeight(3)
+            line(pointA.x+25, pointA.y, pointB.x-10, pointB.y);
+           line(pointA.x+25,pointA.y,pointB.x+30,pointB.y-3);
+            image(this.sling3,pointA.x+25,pointA.y-10,15,30);
+           }
+            pop();
         }
     }
     
